a53a2d948ef9acaa28cfc1abffbc483ab9a3d918
Bharat Dua <bharatdua13@gmail.com>
2024-09-02 10:30:55 +0530

    Built the custom contex provider component

---
+++ src/PostContext.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+// const PostProvider = createContext();
+const PostContext = createContext();
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 10 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export { PostProvider, PostContext };
